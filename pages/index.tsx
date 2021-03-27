556e1dfd60ce7e59c168c50f2c6b4d7eeb69a9cd
kdilot <unknown>
2021-03-27 10:09:45 +0900

    [#5] 인트로페이지 색상미적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,51 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import Router from 'next/router'
 
 export default function Home() {
+  const [range, setRange] = useState<any>('0%')
   useEffect(() => {
     setTimeout(() => {
-      Router.push('/main')
-    }, 3000)
+      setRange('100%')
+    }, 1000)
   }, [])
 
   return (
     <Wrapper>
-      <Title>INTRO</Title>
+      <Title data-text="MINT" range={range} data-font="eng">
+        MINT
+      </Title>
     </Wrapper>
   )
 }
 
-const Wrapper = styled.div`
+const Wrapper = styled.section`
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
   width: 100%;
   height: 100%;
-  user-select: none;
+  /* user-select: none; */
 `
 
-const Title = styled.h1`
-  font-size: 10rem;
+const Title = styled.div<any>`
+  font-size: 7rem;
+  font-weight: bold;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  color: #000;
+
+  &::before {
+    content: attr(data-text);
+    position: absolute;
+    top: 0;
+    left: 0;
+    color: red;
+    width: ${(props) => props.range};
+    height: 100%;
+    overflow: hidden;
+    transition: all 3s ease-in;
+  }
 `
