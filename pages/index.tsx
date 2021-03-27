7b6d65612c64f5051a9a65ab17ca03eba4732a5a
kdilot <kdilotdev@gmail.com>
2021-03-27 11:13:35 +0900

    [#5] 인트로페이지 색상미적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import RoutÎer from 'next/router'
 
 export default function Home() {
   const [range, setRange] = useState<any>('0%')
