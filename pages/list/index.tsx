375e8925a0a0bd12f70d3d85bcd39f945330c80e
kdilot <kdilotdev@gmail.com>
2021-04-07 08:46:21 +0900

    [#16] 상품 공통 컴포넌트 구현

--- pages/list/index.tsx
+++ pages/list/index.tsx
@@ -1,4 +1,5 @@
 import Divider from '@src/components/divider'
+import Goods from '@src/components/goods'
 import Header from '@src/components/header'
 import styled from 'styled-components'
 
@@ -6,28 +7,64 @@ const List: React.FC = () => {
   return (
     <>
       <Header title="MINT" />
-      <TopGoods>top</TopGoods>
+      <TopGoods>
+        <Goods />
+        <Goods />
+        <Goods />
+        <Goods />
+        <Goods />
+        <Goods />
+        <Goods />
+      </TopGoods>
       <Divider />
-      <BottomGoods>bottom</BottomGoods>
+      <BottomGoods>
+        <Goods type="column" />
+        <Goods type="column" />
+        <Goods type="column" />
+        <Goods type="column" />
+        <Goods type="column" />
+        <Goods type="column" />
+        <Goods type="column" />
+        <Goods type="column" />
+        <Goods type="column" />
+        <Goods type="column" />
+        <Goods type="column" />
+        <Goods type="column" />
+        <Goods type="column" />
+        <Goods type="column" />
+        <Goods type="column" />
+        <Goods type="column" />
+      </BottomGoods>
     </>
   )
 }
 
 const TopGoods = styled.article`
   width: 100%;
-  height: 300px;
+  height: auto;
   display: flex;
-  justify-content: center;
-  align-items: center;
   margin-top: 50px;
+  overflow: auto;
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+  &::-webkit-scrollbar {
+    display: none; /* Chrome, Safari, Opera*/
+  }
 `
 
 const BottomGoods = styled.article`
   width: 100%;
   height: 100%;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+  overflow: auto;
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+  &::-webkit-scrollbar {
+    display: none; /* Chrome, Safari, Opera*/
+  }
 `
 
 export default List
